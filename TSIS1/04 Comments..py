cee3b8d60894842820e83831cdeefa5a4b6345e2
Darkhan2123 <aukenov849@gmail.com>
2023-01-19 18:31:12 +0300

    Bunch of 0 and 1

---
+++ TSIS1/04 Comments..py
@@ -0,0 +1,11 @@
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
+
+#or you can do that by this symbol '#'
+
+#Comment
+#Comment
---
+++ TSIS1/05 Variables.py
@@ -0,0 +1,9 @@
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+
+a = 5
+b = "John"
+print(type(a))
+print(type(b))
