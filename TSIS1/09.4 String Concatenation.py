399d6353bd3a430e4f8927cffd8594f00792e5ba
Darkhan2123 <aukenov849@gmail.com>
2023-01-21 17:21:55 +0300

    Bunch of 0 and 1

---
+++ TSIS1/09.4 String Concatenation.py
@@ -0,0 +1,11 @@
+a = "Hello"
+b = "World"
+c = a + b
+print(c) # Prints HelloWorld
+
+
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c) #Prints Hello World with space
+
---
+++ TSIS1/09.6 Escape Characters.py
@@ -0,0 +1,5 @@
+txt = "We are the so-called \"Vikings\" from the north."
+
+print (txt)
+#! use \" to do that
+# U cant do like this txt = "We are the so-called "Vikings" from the north."
